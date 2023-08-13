f7a5aee7a077be4fc1f96e689bc186e79b5eb122
NikolayKuz <nikolaykuz@yahoo.com>
2023-08-13 16:10:30 +0300

    Before adapting to IOS15

---
+++ Scrumdinger/Models/View/ThemePicker.swift
@@ -0,0 +1,32 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by Ник К on 11.08.2023.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+    
+    var body: some View {
+        if #available(iOS 16.0, *) {
+            Picker("Theme", selection: $selection) {
+                ForEach(Theme.allCases) { theme in
+                    ThemeView(theme: theme)
+                        .tag(theme)
+                }
+            }
+            .pickerStyle(.navigationLink)
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+}
+
+struct ThemePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemePicker(selection: .constant(.periwinkle))
+    }
+}
---
+++ Scrumdinger/Models/View/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Ник К on 10.08.2023.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
